4dcca0b5ba0a3ca5967989c4c6988480e9341340
Alexander Shvetsov <alex85.ekb@gmail.com>
2021-02-01 09:59:52 +0500

    lesson one done

--- standfordCourseOne/ViewController.swift
+++ standfordCourseOne/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    // change label on click card
+    var touches = 0 {
+        didSet {
+            touchLabel.text = "Touches: \(touches)"
+        }
     }
+    
+    // change values of card if click on card
+    func flipButton(emoji: String, button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = #colorLiteral(red: 0, green: 0.6312296391, blue: 0.8726374507, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+    
+    // values of cards
+    let emojiCollection = ["🦊", "🐼", "🦊", "🐼"]
 
-
+    @IBOutlet var buttonCollection: [UIButton]!
+    
+    @IBOutlet weak var touchLabel: UILabel!
+    
+    @IBAction func cardButton(_ sender: UIButton) {
+        
+        // change label on click card
+        touches += 1
+        
+        // change value of card if click on card
+        if let buttonIndex = buttonCollection.firstIndex(of: sender) {
+            flipButton(emoji: emojiCollection[buttonIndex], button: sender)
+        }
+        
+    }
+    
 }
 
